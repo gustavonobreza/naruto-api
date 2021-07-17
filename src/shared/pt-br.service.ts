e2f578d53fa93befe843bf751307d0966d6c7554
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-07-17 02:30:40 -0300

    lazy way to implements a cache

--- src/shared/pt-br.service.ts
+++ src/shared/pt-br.service.ts
@@ -2,21 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { readFileSync } from 'fs';
 import { Character } from './character.entity';
 
+let cache = null;
+
 @Injectable()
 export class PtBrService {
   async getAll(): Promise<Character[]> {
-    const raw = await JSON.parse(
-      readFileSync('src/shared/data/pt-br-V2a.json', {
-        encoding: 'utf8',
-      }).toString(),
-    );
+    if (!cache) {
+      const raw = await JSON.parse(
+        readFileSync('src/shared/data/pt-br-V2a.json', {
+          encoding: 'utf8',
+        }).toString(),
+      );
+      cache = raw;
+    }
 
-    const names = Object.keys(raw);
+    const names = Object.keys(cache);
 
     const serialized = names.map((name, ind) => ({
       id: ind + 1,
       name,
-      ...raw[name],
+      ...cache[name],
     }));
 
     // console.log(serialized.find(({ name }) => name === 'Naruto Uzumaki'));
