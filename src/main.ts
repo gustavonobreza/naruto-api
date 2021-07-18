28e268aedd1a7387e6346a54e64711e8bd9337b5
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-07-17 23:51:29 -0300

    Add: ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
   await app.listen(3000);
 }
