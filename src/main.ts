6743a5464fde6f56f6fda52a9d2d91c193d12784
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-07-30 02:17:11 -0300

    fix: env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
-  await app.listen(+process.env.NODE_PORT || 3000);
+  await app.listen(+process.env.PORT || 3000);
 }
 bootstrap();
