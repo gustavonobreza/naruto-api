84245cf5e8a797e1cf4647723854a05bd955c075
Gustavo Alexandre Nobre Mesquita <gnobremesquita@gmail.com>
2023-07-02 10:00:29 -0300

    fix: import service

--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { CharactersService } from './characters.service';
 import { CharactersController } from './characters.controller';
-import { PtBrService } from 'src/shared/pt-br.service';
-import { EnService } from 'src/shared/en.service';
+import { CharactersService } from './characters.service';
 
 @Module({
-  imports: [PtBrService, EnService],
+  imports: [],
   controllers: [CharactersController],
   providers: [CharactersService],
 })
