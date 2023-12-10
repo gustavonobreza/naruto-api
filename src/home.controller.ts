b7839e691191c82876a5ee0e73bb4fc1f749ca55
Gustavo Alexandre Nobre Mesquita <gustavonobremesquita@gmail.com>
2023-12-10 13:41:24 -0300

    chore: add any anotation

--- src/home.controller.ts
+++ src/home.controller.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 
-export function homeController(app: FastifyInstance, opts, done) {
+export function homeController(app: FastifyInstance, _: any, done: any) {
   app.get('/', (_, reply) => {
     reply.redirect(303, 'api/v1/characters');
   });
