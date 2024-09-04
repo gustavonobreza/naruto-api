ab7e29d0e9be7844407fc8baf2a143d5d0c65b7d
Gustavo A. Nobre Mesquita <gustavonobremesquita#gmail.com>
2024-09-03 23:30:35 -0300

    change

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import cors from '@fastify/cors';
-import fastify from 'fastify';
-import { charactersController } from './api/characters/characters.controller';
-import { clansController } from './api/clans/clans.controller';
-import { homeController } from './api/home.controller';
+import cors from "@fastify/cors";
+import fastify from "fastify";
+import { charactersController } from "./api/characters/characters.controller";
+import { clansController } from "./api/clans/clans.controller";
+import { homeController } from "./api/home.controller";
 
 async function bootstrap() {
   const app = fastify({ logger: true });
   app.register(cors);
   app.register(homeController);
-  app.register(charactersController, { prefix: '/api/v1/characters' });
-  app.register(clansController, { prefix: '/api/v1/clans' });
+  app.register(charactersController, { prefix: "/api/v1/characters" });
+  app.register(clansController, { prefix: "/api/v1/clans" });
 
-  await app.listen({ port: parseInt(process.env.PORT as string) || 3000 });
+  await app.listen({ port: 3000 });
 }
 bootstrap();
